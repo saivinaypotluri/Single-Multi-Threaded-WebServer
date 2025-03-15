0c2de2e466e9495a1694e08a35f31fcfb03aba30
Sai Vinay Potluri <112097146+saivinaypotluri@users.noreply.github.com>
2025-03-15 17:23:24 -0400

    Add files via upload

---
+++ Single-Threaded/Server.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Server{
+
+    public void run() throws IOException{
+        int port=8010;
+        ServerSocket socket = new ServerSocket(port);
+        try {
+            socket.setSoTimeout(10000);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                System.out.println("Server is listening on port"+ port);
+                Socket acceptedConnection = socket.accept(); 
+                System.out.println("Connection accepted from client"+acceptedConnection.getRemoteSocketAddress()); 
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                toClient.println("Hello from the Server");
+                toClient.close();
+                fromClient.close();
+                acceptedConnection.close();   
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+
+    }
+    public static void main(String [] args) 
+    {
+        Server server = new Server();
+        try {
+            server.run();
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+}
